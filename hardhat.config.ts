c29d2fd1c7420e8bb8144442cebed77088777e6d
m1ss1onBest <m1poisthbexxxx@gmail.com>
2024-08-07 19:02:20 +0300

    test: generate docs

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,14 @@
+import 'solidity-docgen';
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+require("solidity-docgen");
+
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  docgen: {
+    outputDir: "./docs",
+  }
 };
 
 export default config;
